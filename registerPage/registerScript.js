6bb75147ffe1bf1540214f168afb65e2b9489be1
mateusyseko <mateusyst@gmail.com>
2025-02-15 09:50:17 +0000

    Criação de página de registro e alterações de fonte de letra na login page

--- registerPage/registerScript.js
+++ registerPage/registerScript.js
@@ -4,10 +4,10 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     const errorMessage = document.getElementById('error-message');
 
     if (password !== confirmPassword) {
-        errorMessage.textContent = 'Passwords do not match!';
+        errorMessage.textContent = 'As palavras-passe não são iguais!';
         event.preventDefault();
     } else if (password.length < 8 || !/\d/.test(password) || !/[!@#$%^&*]/.test(password)) {
-        errorMessage.textContent = 'Password must be at least 8 characters long and include a number and a special character!';
+        errorMessage.textContent = 'A palavra-passe deve ter pelo menos 8 caracteres e incluir um número e um caractere especial!';
         event.preventDefault();
     } else {
         errorMessage.textContent = '';
